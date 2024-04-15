a6476f50404f53d1a913be5d95cfc15a09a0dc9c
Ruslan Ivanov <rusivary@gmail.com>
2024-04-15 13:24:10 +0400

    feature/visitors-dialog | added components in visitors dialog

--- src/components/VisitorsDialog.vue
+++ src/components/VisitorsDialog.vue
@@ -1,14 +1,39 @@
 <template>
   <div v-if="isOpen" class="modal">
     <div class="modal-content">
-      <span class="close" @click="closeModal">×</span>
-      <p>Здесь ваш контент</p>
+      <button class="close" @click="closeModal">
+        <CloseButton />
+      </button>
+      <div class="modal-form">
+        <div class="modal-form--row">
+          <span>ФИО</span>
+          <input />
+        </div>
+        <div class="modal-form--row">
+          <span>Компания</span>
+          <input />
+        </div>
+        <div class="modal-form--row">
+          <span>Группа</span>
+          <input />
+        </div>
+        <div class="modal-form--row">
+          <span>Присутствие</span>
+          <input type="checkbox" />
+        </div>
+        
+        <div class="modal-button-group">
+          <button>Добавить</button>
+          <button>Закрыть</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, defineExpose } from 'vue';
+import CloseButton from '../assets/CloseButton.vue'
 
 const isOpen = ref(false);
 
@@ -36,28 +61,48 @@ defineExpose({ openModal });
   background-color: rgb(0,0,0);
   background-color: rgba(0,0,0,0.3);
   backdrop-filter: blur(2px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .modal-content {
   background-color: #FFFFFF;
   border-radius: 30px;
-  margin: 15% auto;
-  padding: 20px;
+  margin: 0;
+  padding: 66px 20px;
   width: 80%;
+  display: flex;
+  justify-content: center;
+  position: relative;
 }
 
 .close {
-  color: #aaa;
-  float: right;
-  font-size: 28px;
-  font-weight: bold;
+  display: flex;
+  background: none;
+  border: none;
+  padding: 0;
+  cursor: pointer;
+  position: absolute;
+  top: 20px;
+  right: 20px;
 }
 
-.close:hover,
-.close:focus {
-  color: black;
-  text-decoration: none;
-  cursor: pointer;
+.modal-form {
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+  width: 30vw;
+}
+
+.modal-form--row {
+  display: flex;
+  justify-content: space-between;
+}
+
+.modal-button-group {
+  display: flex;
+  gap: 34px;
 }
 </style>
 
